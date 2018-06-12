37318eb4dd6a6a45b7068223d81069ca70c2fc68
jhonmunoz <jhonathanmunoz@gmail.com>
2018-06-12 11:21:28 -0400

    Removing unnecessary files from project

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_203152) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
