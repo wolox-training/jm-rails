f46916e220a4c221b65dc613996fca8e7b335721
jhonmunoz <jhonathanmunoz@gmail.com>
2018-06-13 15:03:15 -0400

    rubocop offenses fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
 end
