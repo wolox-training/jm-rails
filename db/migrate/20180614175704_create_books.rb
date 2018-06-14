3583964889813c0f77ffbaea58cd2b1f86969971
jhonmunoz <jhonathanmunoz@gmail.com>
2018-06-14 16:24:14 -0400

    Using snake_case. Validations added to Book Model

--- db/migrate/20180614175704_create_books.rb
+++ db/migrate/20180614175704_create_books.rb
@@ -1,12 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :Genre, null: false
-      t.string :Author, null: false
-      t.string :Image, null: false
-      t.string :Title, null: false
-      t.string :Publisher, null: false
-      t.string :Year, null: false
+      t.string :genre, null: false
+      t.string :author, null: false
+      t.string :image, null: false
+      t.string :title, null: false
+      t.string :publisher, null: false
+      t.string :year, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 2018_06_14_175704) do
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.string "Genre", null: false
-    t.string "Author", null: false
-    t.string "Image", null: false
-    t.string "Title", null: false
-    t.string "Publisher", null: false
-    t.string "Year", null: false
+    t.string "genre", null: false
+    t.string "author", null: false
+    t.string "image", null: false
+    t.string "title", null: false
+    t.string "publisher", null: false
+    t.string "year", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
