9db423f4870f3601ead10a13c506670d50442c57
jhonmunoz <jhonathanmunoz@gmail.com>
2018-06-27 19:25:17 -0400

    RentMail configured

---
+++ app/mailers/rent_mailer.rb
@@ -0,0 +1,12 @@
+class RentMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.rent_mailer.new_rent_notification.subject
+  #
+  def new_rent_notification(rent)
+    @rent = rent
+    mail to: @rent.user.email
+  end
+end
