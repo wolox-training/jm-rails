766dbc899b9a89869c6b5642d863c5d3d8934fb0
jhonmunoz <jhonathanmunoz@gmail.com>
2018-06-27 21:14:17 -0400

    fixing rubocop offenses

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,5 +1,6 @@
-class RentMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class RentMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class EmailWorker
   include Sidekiq::Worker
 
