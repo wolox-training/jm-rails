abdf6efa09580269b32c013f3d289696e130150a
jhonmunoz <jhonathanmunoz@gmail.com>
2018-06-29 17:02:27 -0400

    configurinf activeAdmin

---
+++ app/admin/books.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register Book do
+  permit_params :genre, :author, :image, :title, :publisher, :year
+
+  index do
+    selectable_column
+    id_column
+    column :genre
+    column :author
+    column :image
+    column :title
+    column :publisher
+    column :year
+    actions
+  end
+
+  filter :genre
+  filter :author
+  filter :image
+  filter :title
+  filter :publisher
+  filter :year
+
+end
---
+++ app/admin/rents.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Rent do
+permit_params :user, :book, :from, :to
+
+index do
+  selectable_column
+  id_column
+  column :user
+  column :book
+  column :from
+  column :to
+  actions
+end
+
+filter :user, as: :select, collection: User.all.pluck(:name, :id)
+filter :book
+filter :from
+filter :to
+end
