a2cfea2c4a949b2bd9629e007e55864ae59a8838
jhonmunoz <jhonathanmunoz@gmail.com>
2018-07-10 12:36:30 -0400

    Using FactoryBot.attributes_for on hash parmas build

--- spec/controllers/api/v1/book_suggestions_controller_spec.rb
+++ spec/controllers/api/v1/book_suggestions_controller_spec.rb
@@ -5,24 +5,14 @@ require 'rails_helper'
 describe Api::V1::BookSuggestionsController, type: :controller do
   describe 'POST #create' do
     context 'when creating a valid book suggestion' do
-      let(:valid_params) do
-        {
-          book_suggestion: {
-            author:    'Fake Name Lastname',
-            link:    'http://www.fakeurl.com',
-            title:    'Fake Title',
-            editorial:  'Fake Editorial',
-            price:    100,
-            publisher:  'Fake Publisher',
-            year:    2010
-          }
-        }
-      end
+      let(:valid_params) { { book_suggestion: FactoryBot.attributes_for(:book_suggestion) } }
+
       it 'creates a new suggestion' do
         expect do
           post :create, params: valid_params
         end.to change { BookSuggestion.count }.by(1)
       end
+
       it 'responds with 201 status' do
         post :create, params: valid_params
         expect(response).to have_http_status(:created)
@@ -31,17 +21,8 @@ describe Api::V1::BookSuggestionsController, type: :controller do
 
     context 'when creating an invalid book suggestion' do
       let(:invalid_params) do
-        {
-          book_suggestion: {
-            author:      nil,
-            link:       'http://www.fakeurl.com',
-            title:      'Fake Title',
-            editorial:  'Fake Editorial',
-            price:      100,
-            publisher:  'Fake Publisher',
-            year:       2010
-          }
-        }
+        { book_suggestion: FactoryBot.attributes_for(:book_suggestion,
+                                                     author: nil) }
       end
       before do
         post :create, params: invalid_params
